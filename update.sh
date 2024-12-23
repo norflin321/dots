18c0238d8b731e79de7cd46f7a27d5fda620645b
Anton Kurtin <norflin321@gmail.com>
2024-12-23 05:29:16 +0300

    update

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 git rm --ignore-unmatch .zshrc
+git rm --ignore-unmatch .p10k.zsh
 git rm --ignore-unmatch .gitconfig
 git rm --ignore-unmatch -rf ./nvim
 
 cp ~/.zshrc ./
+cp ~/.p10k.zsh ./
 cp ~/.gitconfig ./
 cp -R ~/.config/nvim ./
 
