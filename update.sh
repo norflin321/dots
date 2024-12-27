3c7e0adc652766450e1344d75bb1fd08fe54d82c
Anton Kurtin <norflin321@gmail.com>
2024-12-27 06:16:57 +0300

    update

--- update.sh
+++ update.sh
@@ -5,11 +5,13 @@ git rm --ignore-unmatch .p10k.zsh
 git rm --ignore-unmatch .gitconfig
 git rm --ignore-unmatch -rf ./nvim
 git rm --ignore-unmatch -rf ./kitty
+git rm --ignore-unmatch -rf ./ghostty
 
 cp ~/.zshrc ./
 cp ~/.p10k.zsh ./
 cp ~/.gitconfig ./
 cp -R ~/.config/nvim ./
 cp -R ~/.config/kitty ./
+cp -R ~/.config/ghostty ./
 
 git add . && git commit -m "update" && git push
